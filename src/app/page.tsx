b20db5a1d726240d217dad3ba4c8af06d5de24ef
spooderman11 <balls93729@gmail.com>
2024-11-11 21:46:47 +0200

    Remove Discord API route and associated profile component to simplify codebase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -57,7 +57,6 @@ import {
 } from "react-icons/si";
 import Link from "next/link";
 import Image from "next/image";
-import DiscordProfile from "@/components/discord-profile";
 
 // Move AnimatedStars component definition to the top
 function AnimatedStars() {
@@ -646,7 +645,6 @@ export default function MinimalistPortfolio() {
           </motion.div>
         </motion.div>
         <NowPlaying />
-        <DiscordProfile userId="1260750149446013090" />
       </div>
     </div>
   );
